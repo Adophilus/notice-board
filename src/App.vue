f82b62e9aa0ea42d585a0a5ef971e899f75c72e6
Adophilus <uchenna19of@gmail.com>
2022-12-11 15:36:07 +0100

    switching to vite

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,83 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
-import PouchDB from "pouchdb"
-import PouchdbFind from "pouchdb-find"
-import Admin from "@/models/Admin"
-import Student from "@/models/Student"
+import PouchDB from "pouchdb";
+import PouchdbFind from "pouchdb-find";
+import Admin from "@/models/Admin";
+import Student from "@/models/Student";
 
-PouchDB.plugin(PouchdbFind)
+console.log("jijifjo");
+PouchDB.plugin(PouchdbFind);
 
 export default {
   name: "App",
-  data () {
+  data() {
     return {
       db: null,
       project: {
         creator: "Valentine",
         yearCreated: 2022,
-        title: "Notice Board"
+        title: "Notice Board",
       },
-      dbWatchers: []
-    }
+      dbWatchers: [],
+    };
   },
   methods: {
-    async checkInstallation () {
-      if (((await Admin.get(this.$root.db)).length) > 0) {
-        const admin = new Admin(this.$root.db, { username: "admin", password: "admin@mail.com", email: "admin@mail.com" })
-        await admin.save()
+    async checkInstallation() {
+      console.log(this.$store);
+      if ((await Admin.get(this.$root.db)).length > 0) {
+        const admin = new Admin(this.$root.db, {
+          username: "admin",
+          password: "admin@mail.com",
+          email: "admin@mail.com",
+        });
+        await admin.save();
       }
     },
-    async checkLogin () {
-      if (!localStorage.user) {
-        this.$router.push({ name: "LoginView" })
-      }
-      else {
-        await this.setUser()
+    async checkLogin() {
+      if (this.$store.getters.user._id) {
+        this.$router.push({ name: "LoginView" });
+      } else {
+        await this.setUser();
       }
     },
-    async setUser () {
-      let user
+    async setUser() {
+      let user;
       if (Admin.is(this.$store.getters.user._id)) {
-        user = await Admin.get(this.$root.db, { id: Admin.split(this.$store.getters.user._id) })
-      }
-      else {
-        user = await Student.get(this.$root.db, { id: Student.split(this.$store.getters.user._id) })
+        user = await Admin.get(this.$root.db, {
+          id: Admin.split(this.$store.getters.user._id),
+        });
+      } else {
+        user = await Student.get(this.$root.db, {
+          id: Student.split(this.$store.getters.user._id),
+        });
       }
 
-      this.$store.commit("setUser", user)
-    }
+      this.$store.commit("setUser", user);
+    },
   },
-  created () {
-    document.title = this.project.title
+  created() {
+    document.title = this.project.title;
   },
-  async mounted () {
+  async mounted() {
     // this.db = new PouchDB("notice-board")
     // await this.db.destroy()
-    this.db = new PouchDB("notice-board")
-    this.db.changes({
-      live: true,
-      since: "now"
-      // include_docs: true
-    }).on("change", (change) => {
-      this.dbWatchers.forEach((method) => method(change))
-    })
+    this.db = new PouchDB("notice-board");
+    this.db
+      .changes({
+        live: true,
+        since: "now",
+        // include_docs: true
+      })
+      .on("change", (change) => {
+        this.dbWatchers.forEach((method) => method(change));
+      });
 
-    await this.checkInstallation()
-    await this.checkLogin()
-  }
-}
+    console.log("hi");
+    await this.checkInstallation();
+    await this.checkLogin();
+  },
+};
 </script>
